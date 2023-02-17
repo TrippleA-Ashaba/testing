a844fe1c3f174450d42a1e893b603c7e30da315e
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-02-18 00:06:46 +0300

    TDD cycle 2 GREEN

--- test/test.js
+++ test/test.js
@@ -5,6 +5,7 @@ describe('Multiply function testing', () => {
   it('Failing multiply test', () => {
     assert.equal(multiply(1, 1), 1);
   });
+
   it('Testing 2x2 ==4', () => {
     assert.equal(multiply(2, 2), 4);
   });
