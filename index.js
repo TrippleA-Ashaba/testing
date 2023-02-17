746ac2282c3ec51ad156cd3c55121dae2fef120b
Ashaba Aheebwa Albert <102288573+TrippleA-Ashaba@users.noreply.github.com>
2023-02-18 00:27:52 +0300

    TDD cycle if languange didn't support multiply

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+// function multiply(a, b) {
+//   return a * b;
+// }
+
 function multiply(a, b) {
-  return a * b;
+  let result = 0;
+  for (let i = 0; i < b; i += 1) {
+    result += a;
+  }
+  return result;
 }
 
 export { multiply };
